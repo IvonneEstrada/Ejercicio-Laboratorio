3b3ac00936ac1898e108f1af2ae99a09d8afdd8a
IvonneEstrada <ivonne_montserrat@outlook.com>
2018-08-30 18:23:59 -0500

    Se agrego el metodo imrpimir lista, dar Baja e inscribir de alumnos

--- Laboratorio.java
+++ Laboratorio.java
@@ -8,9 +8,30 @@ public class Laboratorio
         alumnos = new ArrayList<Alumno>();
     }
     
-    public void incribir(String nombre, int clave)
+    public void inscribir(String nombre, int clave)
     {
-        Alumno alumno = new Alumno(nombre,clave);
-        
+        Alumno agregar = new Alumno(nombre,clave);
+        alumnos.add(agregar);
+    }
+    
+    public void darBaja(int claveBaja)
+    {
+        for(int i = 0; i < alumnos.size(); i++)
+        {
+            Alumno alum = alumnos.get(i);
+            if(alum.dimeClave() == claveBaja)
+            {
+                alumnos.remove(i);
+            }
+        }
+    }
+    
+    public void imprimirLista()
+    {
+        for(int i = 0; i < alumnos.size(); i++)
+        {
+            Alumno alum = alumnos.get(i);
+            System.out.println(alum);
+        }
     }
 }
